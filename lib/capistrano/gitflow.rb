2ce56be420a9dc2c42141a1908ff116998e59a07
Marc Leglise <mleglise@gmail.com>
2012-06-20 12:42:08 -0700

    New environment variable CAP_NOPROMPT allows Jenkins to bypass the y/n confirmations.

--- lib/capistrano/gitflow.rb
+++ lib/capistrano/gitflow.rb
@@ -168,15 +168,19 @@ Please make sure you have pulled and pushed all code before deploying:
 
             if new_production_tag == last_production_tag
               puts "Not re-tagging #{last_production_tag} because it already exists"
-             really_deploy = Capistrano::CLI.ui.ask("Do you really want to deploy #{last_production_tag}? [y/N]").to_url
+              if ENV['CAP_NOPROMPT'].nil? or ENV['CAP_NOPROMPT'] == ""
+                really_deploy = Capistrano::CLI.ui.ask("Do you really want to deploy #{last_production_tag}? [y/N]").to_url
 
-             exit(1) unless really_deploy =~ /^[Yy]$/
+                exit(1) unless really_deploy =~ /^[Yy]$/
+              end
             else
               puts "Preparing to promote staging tag '#{promote_to_production_tag}' to '#{new_production_tag}'"
               unless capistrano_configuration[:tag]
-                really_deploy = Capistrano::CLI.ui.ask("Do you really want to deploy #{new_production_tag}? [y/N]").to_url
+                if ENV['CAP_NOPROMPT'].nil? or ENV['CAP_NOPROMPT'] == ""
+                  really_deploy = Capistrano::CLI.ui.ask("Do you really want to deploy #{new_production_tag}? [y/N]").to_url
 
-                exit(1) unless really_deploy =~ /^[Yy]$/
+                  exit(1) unless really_deploy =~ /^[Yy]$/
+                end
               end
               puts "Promoting staging tag #{promote_to_production_tag} to production as '#{new_production_tag}'"
               system "git tag -a -m 'tagging current code for deployment to production' #{new_production_tag} #{promote_to_production_tag}"
